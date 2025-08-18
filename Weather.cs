e17b48dc991771bd3873634a63a5ab944cca0f4f
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-18 16:26:34 +0300

    Add weather model

---
+++ Weather.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace FTSWeatherMonitoringAndReporting;
+
+[XmlRoot("WeatherData")]
+public class Weather
+{
+    public string Location { get; set; }
+    public int Temperature { get; set; }
+    public int Humidity { get; set; }
+
+    public override string ToString()
+    {
+        return $"Location: {Location}, Temperature: {Temperature}, Humidity: {Humidity}";
+    }
+}
