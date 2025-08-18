230be75a4a42cfc6afd60d8f25bbef44c82fcad7
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-18 16:27:40 +0300

    Add configuration file settings for bots

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
 ﻿namespace FTSWeatherMonitoringAndReporting;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var config = new ConfigurationBuilder()
+            .SetBasePath(FindProjectPath())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var jsonParser = new JsonParser();
+        var res = jsonParser.Parse("{\"Location\": \"City Name\", \"Temperature\": 35, \"Humidity\": 40}");
+        Console.WriteLine(res);
+        var xmlParser = new XmlParser();
+        var res1 = xmlParser.Parse("<WeatherData><Location>City Name</Location><Temperature>32</Temperature><Humidity>40</Humidity></WeatherData>");
+        Console.WriteLine(res1);
+
+        Console.WriteLine(config["Rainbot:humidityThreshold"]);
+    }
+    private static string FindProjectPath()
+    {
+        var current = Directory.GetCurrentDirectory();
+        while (current != null && !File.Exists(Path.Combine(current, "Program.cs")))
+        {
+            current = Directory.GetParent(current)?.FullName;
+        }
+        return current!;
     }
 }
