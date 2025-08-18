94a60d31cc0e65069881e0e1b0cfb068429f1924
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-18 16:27:20 +0300

    Add parsing exception

---
+++ ParsingException.cs
@@ -0,0 +1,7 @@
+﻿namespace FTSWeatherMonitoringAndReporting;
+
+public class ParsingException : Exception
+{
+    public ParsingException(string format, Exception inner) 
+        : base($"Failed to parse {format} input", inner) {}
+}
