079b893a1b0736c4b93a2dbdea6d5cbc22f64f61
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-20 17:04:48 +0300

    Add: implement weather bots models, factories

---
+++ Features/WeatherBots/Strategies/Factory/SunBotStrategyFactory.cs
@@ -0,0 +1,15 @@
+﻿using FTSWeatherMonitoringAndReporting.Configuration;
+using FTSWeatherMonitoringAndReporting.Features.WeatherBots.Models.Enums;
+
+namespace FTSWeatherMonitoringAndReporting.Features.WeatherBots.Strategies.Factory;
+
+public class SunBotStrategyFactory : IBotStrategyFactory
+{
+    public BotType BotType => BotType.SunBot;
+    public BotStrategy? Create(BotConfiguration botConfiguration)
+    {
+        if (!botConfiguration.Enabled) return null;
+        return new SunBotStrategy(nameof(BotType.SunBot), botConfiguration.Message,
+            botConfiguration.TemperatureThreshold);
+    }
+}
