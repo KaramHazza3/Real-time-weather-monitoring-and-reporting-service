0071f6fc17f35b69da1421914456930975c3ee61
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-20 17:06:39 +0300

    Add: implement weather station where the events will be send to bots

---
+++ Features/WeatherStation/WeatherStation.cs
@@ -0,0 +1,42 @@
+﻿using FTSWeatherMonitoringAndReporting.Common.Parsers.Exceptions;
+using FTSWeatherMonitoringAndReporting.Features.WeatherStation.Events;
+using FTSWeatherMonitoringAndReporting.Features.WeatherStation.Providers;
+
+namespace FTSWeatherMonitoringAndReporting.Features.WeatherStation;
+
+public class WeatherStation
+{
+    private readonly IWeatherProvider _weatherProvider;
+
+    public WeatherStation(IWeatherProvider weatherProvider)
+    {
+        _weatherProvider = weatherProvider;
+    }
+    public event EventHandler<WeatherCreatedEventArgs> WeatherCreated;
+
+    public void GetWeather()
+    {
+        while (true)
+        {
+            try
+            {
+                var weatherData = _weatherProvider.GetWeather();
+                if (weatherData == null)
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+                OnWeatherCreated(new WeatherCreatedEventArgs(weatherData));
+            }
+            catch (ParsingException e)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+        }
+    }
+    protected virtual void OnWeatherCreated(WeatherCreatedEventArgs e)
+    {
+        WeatherCreated?.Invoke(this, e);
+    }
+}
