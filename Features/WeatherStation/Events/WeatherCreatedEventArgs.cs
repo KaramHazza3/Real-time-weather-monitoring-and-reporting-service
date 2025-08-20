d9efac8193e2f95f27588e21de733ecebb77db2e
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-20 17:05:52 +0300

    Add: add event arguments to be sent

---
+++ Features/WeatherStation/Events/WeatherCreatedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using FTSWeatherMonitoringAndReporting.Features.WeatherStation.Models;
+
+namespace FTSWeatherMonitoringAndReporting.Features.WeatherStation.Events;
+
+public class WeatherCreatedEventArgs : EventArgs
+{
+    public Weather Weather { get; }
+    public WeatherCreatedEventArgs(Weather weather) => Weather = weather;
+}
